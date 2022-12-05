1ae83574beff5f234314903055557068492a72ce
Vayra73134885 <73134885@isphde.edu.pe>
2022-12-04 22:34:15 -0500

    vesion 6

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootswatch/dist/boostrap.min.css'
+import 'bootswatch/dist/solar/boostrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
